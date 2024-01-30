95182fb0a574fe77b084c1d2bfdbcd64a7501555
Orestis-Antonopoulos <skyscreamer@live.com>
2024-01-30 10:16:14 +0200

    Skills with Icons. Add hover effect and tooltip transition

--- resources/views/mainpages/skills.blade.php
+++ resources/views/mainpages/skills.blade.php
@@ -1,24 +1,141 @@
 <div id="skillssection" 
-class="hidden absolute rounded-b-lg rounded-tr-lg top-[60px] bottom-0 w-full sm:w-[620px] p-[10px]"
+class="hidden absolute rounded-b-lg rounded-tr-lg top-[60px] bottom-0 w-full sm:w-[620px] p-[5px] sm:p-[10px]"
 style="background:#FFFFFF20;">
     <div class="absolute top-[-60px] left-0 right-[180px] h-[60px] sm:w-[440px] rounded-t-lg" style="background:#FFFFFF20;">
-    <h2 class="text-2xl sm:text-4xl text-bold text-center mt-[10px] drop-shadow" style="color:#AAA;">SKILLS</h2>
+    <h2 class="text-2xl sm:text-4xl text-bold text-center mt-[10px] drop-shadow" style="color:#A0A0A0;">SKILLS</h2>
     </div>
     <div class="relative w-full h-full">
-        <div>
-            <span>Programming Languages:</span>
-            <span>PHP, JavaScript, HTML, CSS</span>
+        <div id="Languages">
+            <span>Languages:</span>
+            <div class="flex flex-row gap-[3px] sm:gap-[5px] mt-[1px] mb-[15px]">
+                <div class="w-[60px] h-[60px] sm:w-[80px] sm:h-[80px] rounded flex justify-center items-center p-[5px] group relative" style="background:#FFFFFF50;">
+                    <div class="absolute bottom-full mb-[-10px] clickthrough tooltip-content">
+                        <div class="bg-black text-white text-xs sm:text-base font-bold rounded py-1 px-2 right-0">PHP</div> <!-- Tooltip title -->
+                        <div class="w-3 h-3 bg-black rotate-45 absolute -bottom-1 left-0 right-0 mx-auto" style="z-index:-1;"></div> <!-- Tooltip arrow -->
+                    </div>
+                    <img src="{{ asset('images/skills/pixelphp.png') }}" alt="A pixelated icon of PHP" class="h-[33px] sm:h-[47px] clickthrough" style="image-rendering: pixelated;">
+                </div>
+                <div class="w-[60px] h-[60px] sm:w-[80px] sm:h-[80px] rounded flex justify-center items-center p-[5px] group relative" style="background:#FFFFFF50;">
+                    <div class="absolute bottom-full mb-[-10px] clickthrough tooltip-content">
+                        <div class="bg-black text-white text-xs sm:text-base font-bold rounded py-1 px-2 right-0">JavaScript</div> <!-- Tooltip title -->
+                        <div class="w-3 h-3 bg-black rotate-45 absolute -bottom-1 left-0 right-0 mx-auto" style="z-index:-1;"></div> <!-- Tooltip arrow -->
+                    </div>
+                    <img src="{{ asset('images/skills/pixeljavascript.png') }}" alt="A pixelated icon of Javascript" class="h-[33px] sm:h-[47px] clickthrough" style="image-rendering: pixelated;">
+                </div>
+                <div class="w-[60px] h-[60px] sm:w-[80px] sm:h-[80px] rounded flex justify-center items-center p-[5px] group relative" style="background:#FFFFFF50;">
+                    <div class="absolute bottom-full mb-[-10px] clickthrough tooltip-content">
+                        <div class="bg-black text-white text-xs sm:text-base font-bold rounded py-1 px-2 right-0">Bash</div> <!-- Tooltip title -->
+                        <div class="w-3 h-3 bg-black rotate-45 absolute -bottom-1 left-0 right-0 mx-auto" style="z-index:-1;"></div> <!-- Tooltip arrow -->
+                    </div>
+                    <img src="{{ asset('images/skills/pixelbash.png') }}" alt="A pixelated icon of Bash scripting" class="h-[33px] sm:h-[47px] clickthrough rounded" style="image-rendering: pixelated;">
+                </div>
+                <div class="w-[60px] h-[60px] sm:w-[80px] sm:h-[80px] rounded flex justify-center items-center p-[5px] group relative" style="background:#FFFFFF50;">
+                    <div class="absolute bottom-full mb-[-10px] clickthrough tooltip-content">
+                        <div class="bg-black text-white text-xs sm:text-base font-bold rounded py-1 px-2 right-0">HTML</div> <!-- Tooltip title -->
+                        <div class="w-3 h-3 bg-black rotate-45 absolute -bottom-1 left-0 right-0 mx-auto" style="z-index:-1;"></div> <!-- Tooltip arrow -->
+                    </div>
+                    <img src="{{ asset('images/skills/html.svg') }}" alt="An icon of HTML" class="h-[33px] sm:h-[47px] clickthrough">
+                </div>
+                <div class="w-[60px] h-[60px] sm:w-[80px] sm:h-[80px] rounded flex justify-center items-center p-[5px] group relative" style="background:#FFFFFF50;">
+                    <div class="absolute bottom-full mb-[-10px] clickthrough tooltip-content">
+                        <div class="bg-black text-white text-xs sm:text-base font-bold rounded py-1 px-2 right-0">CSS</div> <!-- Tooltip title -->
+                        <div class="w-3 h-3 bg-black rotate-45 absolute -bottom-1 left-0 right-0 mx-auto" style="z-index:-1;"></div> <!-- Tooltip arrow -->
+                    </div>
+                    <img src="{{ asset('images/skills/css.svg') }}" alt="An icon of CSS" class="h-[33px] sm:h-[47px] clickthrough">
+                </div>
+            </div>
         </div>
-        <div>
-            <span>Frameworks & Libraries:</span>
-            <span>Laravel, Tailwind</span>
+        <div id="Frameworks">
+            <span>Frameworks:</span>
+            <div class="flex flex-row gap-[3px] sm:gap-[5px] mt-[1px] mb-[15px]">
+                <div class="w-[60px] h-[60px] sm:w-[80px] sm:h-[80px] rounded flex justify-center items-center p-[5px] group relative" style="background:#FFFFFF50;">
+                    <div class="absolute bottom-full mb-[-10px] clickthrough tooltip-content">
+                        <div class="bg-black text-white text-xs sm:text-base font-bold rounded py-1 px-2 right-0">Laravel</div> <!-- Tooltip title -->
+                        <div class="w-3 h-3 bg-black rotate-45 absolute -bottom-1 left-0 right-0 mx-auto" style="z-index:-1;"></div> <!-- Tooltip arrow -->
+                    </div>
+                    <img src="{{ asset('images/skills/laravel.svg') }}" alt="An icon of Laravel" class="h-[33px] sm:h-[47px] clickthrough">
+                </div>  
+                <div class="w-[60px] h-[60px] sm:w-[80px] sm:h-[80px] rounded flex justify-center items-center p-[5px] group relative" style="background:#FFFFFF50;">
+                    <div class="absolute bottom-full mb-[-10px] clickthrough tooltip-content">
+                        <div class="bg-black text-white text-xs sm:text-base font-bold rounded py-1 px-2 right-0">Tailwind</div> <!-- Tooltip title -->
+                        <div class="w-3 h-3 bg-black rotate-45 absolute -bottom-1 left-0 right-0 mx-auto" style="z-index:-1;"></div> <!-- Tooltip arrow -->
+                    </div>
+                    <img src="{{ asset('images/skills/tailwind.svg') }}" alt="An icon of Tailwind" class="h-[33px] sm:h-[47px] clickthrough">
+                </div>  
+            </div>
         </div>
-        <div>
+        <div id="Tools&Tech">
             <span>Tools & Technologies:</span>
-            <span>Git, GitHub, VS Code, MySQL, Nginx, Linux, Windows</span>
+            <div class="flex flex-row gap-[3px] sm:gap-[5px] mt-[1px] mb-[15px]">
+                <div class="w-[60px] h-[60px] sm:w-[80px] sm:h-[80px] rounded flex justify-center items-center p-[5px] group relative" style="background:#FFFFFF50;">
+                    <div class="absolute bottom-full mb-[-10px] clickthrough tooltip-content">
+                        <div class="bg-black text-white text-xs sm:text-base font-bold rounded py-1 px-2 right-0">GIT</div> <!-- Tooltip title -->
+                        <div class="w-3 h-3 bg-black rotate-45 absolute -bottom-1 left-0 right-0 mx-auto" style="z-index:-1;"></div> <!-- Tooltip arrow -->
+                    </div>
+                    <img src="{{ asset('images/skills/git.svg') }}" alt="An icon of git" class="h-[33px] sm:h-[47px] clickthrough">
+                </div>
+                <div class="w-[60px] h-[60px] sm:w-[80px] sm:h-[80px] rounded flex justify-center items-center p-[5px] group relative" style="background:#FFFFFF50;">
+                    <div class="absolute bottom-full mb-[-10px] clickthrough tooltip-content">
+                        <div class="bg-black text-white text-xs sm:text-base font-bold rounded py-1 px-2 right-0">VSCode</div> <!-- Tooltip title -->
+                        <div class="w-3 h-3 bg-black rotate-45 absolute -bottom-1 left-0 right-0 mx-auto" style="z-index:-1;"></div> <!-- Tooltip arrow -->
+                    </div>
+                    <img src="{{ asset('images/skills/vscode.svg') }}" alt="An icon of vscode" class="h-[33px] sm:h-[47px] clickthrough">
+                </div>
+                <div class="w-[60px] h-[60px] sm:w-[80px] sm:h-[80px] rounded flex justify-center items-center p-[5px] group relative" style="background:#FFFFFF50;">
+                    <div class="absolute bottom-full mb-[-10px] clickthrough tooltip-content">
+                        <div class="bg-black text-white text-xs sm:text-base font-bold rounded py-1 px-2 right-0">MySQL</div> <!-- Tooltip title -->
+                        <div class="w-3 h-3 bg-black rotate-45 absolute -bottom-1 left-0 right-0 mx-auto" style="z-index:-1;"></div> <!-- Tooltip arrow -->
+                    </div>
+                    <img src="{{ asset('images/skills/mysql.svg') }}" alt="An icon of mysql" class="h-[33px] sm:h-[47px] clickthrough">
+                </div>
+                <div class="w-[60px] h-[60px] sm:w-[80px] sm:h-[80px] rounded flex justify-center items-center p-[5px] group relative" style="background:#FFFFFF50;">
+                    <div class="absolute bottom-full mb-[-10px] clickthrough tooltip-content">
+                        <div class="bg-black text-white text-xs sm:text-base font-bold rounded py-1 px-2 right-0">NGiNX</div> <!-- Tooltip title -->
+                        <div class="w-3 h-3 bg-black rotate-45 absolute -bottom-1 left-0 right-0 mx-auto" style="z-index:-1;"></div> <!-- Tooltip arrow -->
+                    </div>
+                    <img src="{{ asset('images/skills/nginx.svg') }}" alt="An icon of nginx" class="h-[33px] sm:h-[47px] clickthrough">
+                </div>
+                <div class="w-[60px] h-[60px] sm:w-[80px] sm:h-[80px] rounded flex justify-center items-center p-[5px] group relative" style="background:#FFFFFF50;">
+                    <div class="absolute bottom-full mb-[-10px] clickthrough tooltip-content">
+                        <div class="bg-black text-white text-xs sm:text-base font-bold rounded py-1 px-2 right-0">Linux</div> <!-- Tooltip title -->
+                        <div class="w-3 h-3 bg-black rotate-45 absolute -bottom-1 left-0 right-0 mx-auto" style="z-index:-1;"></div> <!-- Tooltip arrow -->
+                    </div>
+                    <img src="{{ asset('images/skills/linux.svg') }}" alt="An icon of Linux" class="h-[33px] sm:h-[47px] clickthrough">
+                </div>
+            </div>
         </div>
-        <div>
-            <span>Additional Skills: Excel etc</span>
+        <div id="extras">
+            <span>Additional Skills:</span>
+            <div class="flex flex-row gap-[3px] sm:gap-[5px] mt-[1px] mb-[15px]">
+                <div class="w-[60px] h-[60px] sm:w-[80px] sm:h-[80px] rounded flex justify-center items-center p-[5px] group relative" style="background:#FFFFFF50;">
+                    <div class="absolute bottom-full mb-[-10px] clickthrough tooltip-content">
+                        <div class="bg-black text-white text-xs sm:text-base font-bold rounded py-1 px-2 right-0">Excel</div> <!-- Tooltip title -->
+                        <div class="w-3 h-3 bg-black rotate-45 absolute -bottom-1 left-0 right-0 mx-auto" style="z-index:-1;"></div> <!-- Tooltip arrow -->
+                    </div>
+                    <img src="{{ asset('images/skills/excel.svg') }}" alt="An icon of excel" class="h-[33px] sm:h-[47px] clickthrough">
+                </div>
+                <div class="w-[60px] h-[60px] sm:w-[80px] sm:h-[80px] rounded flex justify-center items-center p-[5px] group relative" style="background:#FFFFFF50;">
+                    <div class="absolute bottom-full mb-[-10px] clickthrough tooltip-content">
+                        <div class="bg-black text-white text-xs sm:text-base font-bold rounded py-1 px-2 right-0">Photoshop</div> <!-- Tooltip title -->
+                        <div class="w-3 h-3 bg-black rotate-45 absolute -bottom-1 left-0 right-0 mx-auto" style="z-index:-1;"></div> <!-- Tooltip arrow -->
+                    </div>
+                    <img src="{{ asset('images/skills/photoshop.svg') }}" alt="An icon of photoshop" class="h-[33px] sm:h-[47px] clickthrough">
+                </div>
+                <div class="w-[60px] h-[60px] sm:w-[80px] sm:h-[80px] rounded flex justify-center items-center p-[5px] group relative" style="background:#FFFFFF50;">
+                    <div class="absolute bottom-full mb-[-10px] clickthrough tooltip-content">
+                        <div class="bg-black text-white text-xs sm:text-base font-bold rounded py-1 px-2 right-0">Premiere</div> <!-- Tooltip title -->
+                        <div class="w-3 h-3 bg-black rotate-45 absolute -bottom-1 left-0 right-0 mx-auto" style="z-index:-1;"></div> <!-- Tooltip arrow -->
+                    </div>
+                    <img src="{{ asset('images/skills/premiere.svg') }}" alt="An icon of premiere" class="h-[33px] sm:h-[47px] clickthrough">
+                </div>
+                <div class="w-[60px] h-[60px] sm:w-[80px] sm:h-[80px] rounded flex justify-center items-center p-[5px] group relative" style="background:#FFFFFF50;">
+                    <div class="absolute bottom-full mb-[-10px] clickthrough tooltip-content">
+                        <div class="bg-black text-white text-xs sm:text-base font-bold rounded py-1 px-2 right-0">Audacity</div> <!-- Tooltip title -->
+                        <div class="w-3 h-3 bg-black rotate-45 absolute -bottom-1 left-0 right-0 mx-auto" style="z-index:-1;"></div> <!-- Tooltip arrow -->
+                    </div>
+                    <img src="{{ asset('images/skills/audacity.svg') }}" alt="An icon of audacity" class="h-[33px] sm:h-[47px] clickthrough">
+                </div>
+            </div>
             <span>Excel, Abode-Photoshop, Adobe-Premiere, Audacity</span>
         </div>
 
