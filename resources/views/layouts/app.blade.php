a423a3ba27981f89b6368dde750f89d2ddee19f0
Orestis-Antonopoulos <skyscreamer@live.com>
2024-03-06 23:35:03 +0200

    Update page title and add meta tags for SEO

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,17 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="{{ mix('css/app.css') }}" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Roboto:400,700&display=swap" rel="stylesheet">
-    <title>Document</title>
+    <title>Orestis Portfolio</title>
+
+    <meta name="title" content="Portfolio of Orestis Antonopoulos">
+    <meta name="description" content="This is the portfolio website of a Jr. Web Dev, Orestis Antonopoulos">
+    <meta property="og:type" content="website">
+    <meta property="og:url" content="https://OrestisAntonopoulos.com">
+    <meta property="og:title" content="Orestis Antonopoulos Portfolio">
+    <meta property="og:description" content="In this portfolio site you can see my projects, skills, contact information and my Cv along with some personal ranting. Hiring me will bestow good fortune upon you!">
+    <meta property="og:image" content="{{ asset('images/mahface.webp') }}">
+
+
 </head>
 <body style="font-family: 'Roboto', sans-serif" class="flex w-full h-full justify-center items-center">
 
