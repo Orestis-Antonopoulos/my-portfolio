a21f1ddd305dc61f3567982254add278854129b9
Orestis-Antonopoulos <skyscreamer@live.com>
2024-03-10 11:48:29 +0200

    Attempt to Fix mobile height once more

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -21,7 +21,7 @@
 
 @include('layouts.bganimation')
 
-<div class="absolute flex w-full h-full justify-center items-center p-[10px] top-[0px]">
+<div class="absolute flex w-full justify-center items-center p-[10px] top-[0px]" style="height: calc(100vh - env(safe-area-inset-bottom, 0px));">
     <div class="container relative w-full sm:w-[620px] h-full">
         @yield('header')
         @include('4sections')
