7b548138796e30b1a6783aed98d3174c77019137
Orestis-Antonopoulos <skyscreamer@live.com>
2024-03-10 22:12:22 +0200

    Preloads SFX

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="{{ mix('css/app.css') }}" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Roboto:400,700&display=swap" rel="stylesheet">
+    <link rel="preload" href="{{ asset('Sounds/backhome.mp3') }}" as="audio">
+    <link rel="preload" href="{{ asset('Sounds/darkmode.mp3') }}" as="audio">
+    <link rel="preload" href="{{ asset('Sounds/lightmode.mp3') }}" as="audio">
+    <link rel="preload" href="{{ asset('Sounds/mainbutton.mp3') }}" as="audio">
+    <link rel="preload" href="{{ asset('Sounds/musicoff.mp3') }}" as="audio">
+    <link rel="preload" href="{{ asset('Sounds/soundoff.mp3') }}" as="audio">
+    <link rel="preload" href="{{ asset('Sounds/soundon.mp3') }}" as="audio">
     <title>Orestis Portfolio</title>
 
     <meta name="title" content="Portfolio of Orestis Antonopoulos">
@@ -24,7 +31,7 @@
     @include('layouts.bganimation')
 </div>
 
-<div class="flex w-full justify-center items-start h-full px-[10px] pt-[10px] pb-[55px] sm:pb-[10px]">
+<div class="flex w-full justify-center items-start h-full px-[10px] pt-[10px] pb-[65px] sm:pb-[10px]">
     <div class="container relative w-full h-full sm:w-[620px]">
 
         @yield('header')
